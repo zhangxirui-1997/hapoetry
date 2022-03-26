6280a8e351cb8fd38453b0e58eef279dd440f403
zhangxirui <2919737573@qq.com>
2022-03-26 16:40:04 +0800

    减少了viewpager2的数量

--- app/src/main/java/com/example/myapplication/ViewPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/ViewPagerAdapter.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.LabeledIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,9 +27,6 @@ public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.Hori
             backgrounds = new ArrayList<>();
             backgrounds.add(android.R.color.holo_blue_bright);
             backgrounds.add(android.R.color.holo_red_dark);
-            backgrounds.add(android.R.color.holo_green_dark);
-            backgrounds.add(android.R.color.holo_orange_light);
-            backgrounds.add(android.R.color.holo_purple);
         }
     }
 
